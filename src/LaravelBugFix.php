625d93aacf248c16db005a80bb292e40b45c236a
appslanka <appslanka@users.noreply.github.com>
2024-06-26 08:29:38 +0000

    Fix styling

--- src/LaravelBugFix.php
+++ src/LaravelBugFix.php
@@ -4,11 +4,10 @@ namespace Appslanka\LaravelBugFix;
 
 use Exception;
 use Illuminate\Contracts\Debug\ExceptionHandler;
-use Illuminate\Foundation\Exceptions\Handler;
 use Illuminate\Support\Facades\Http;
 
-class LaravelBugFix extends ExceptionHandler{
-
+class LaravelBugFix extends ExceptionHandler
+{
     protected $config;
 
     public function __construct($app, $config)
