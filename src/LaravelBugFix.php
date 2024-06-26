cc887d8c540f348dea40aed89cd5829dc235ecbb
appslanka <appslanka@users.noreply.github.com>
2024-06-26 08:55:33 +0000

    Fix styling

--- src/LaravelBugFix.php
+++ src/LaravelBugFix.php
@@ -27,10 +27,10 @@ class LaravelBugFix extends ExceptionHandler
         ];
 
         Http::withHeaders([
-            'Authorization' => 'Bearer '. $this->config['api_key'],
+            'Authorization' => 'Bearer '.$this->config['api_key'],
         ])->post($this->config['api_url'], $payload);
 
-        info("Calling Laravel bug fix reporting >>>");
+        info('Calling Laravel bug fix reporting >>>');
         // Call the parent report method to ensure other reporting is done
         parent::report($e);
     }
