bdeaf9754b9044de93e4878b7c47170f52c450ec
Selva Jo <appslankasoftwaresolutions@gmail.com>
2024-06-26 11:32:52 +0200

    added more parameters

--- src/LaravelBugFix.php
+++ src/LaravelBugFix.php
@@ -5,6 +5,10 @@ namespace Appslanka\LaravelBugFix;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Support\Facades\Http;
 use Throwable;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Session;
 
 class LaravelBugFix extends ExceptionHandler
 {
@@ -24,10 +28,25 @@ class LaravelBugFix extends ExceptionHandler
             'file' => $e->getFile(),
             'line' => $e->getLine(),
             'trace' => $e->getTraceAsString(),
+            'code' => $e->getCode(), // Exception or error code
+            'previous' => $e->getPrevious(), // Previous exception if available
+            'url' => request()->fullUrl(), // Full URL of the request
+            'method' => request()->method(), // HTTP method of the request
+            'headers' => request()->headers->all(), // All request headers
+            'input' => request()->all(), // All request input data
+            'user_agent' => request()->header('User-Agent'), // User agent string
+            'ip' => request()->ip(), // Client IP address
+            'request_time' => now()->toDateTimeString(), // Current timestamp
+            'php_version' => phpversion(), // PHP version running the application
+            'server_params' => $_SERVER, // All server parameters
+            'get_params' => $_GET, // All GET parameters
+            'post_params' => $_POST, // All POST parameters
+            'cookies' => $_COOKIE, // All cookies
+            'session' => session()->all(), // All session data (if using session)
         ];
 
         Http::withHeaders([
-            'Authorization' => 'Bearer '. $this->config['api_key'],
+            'Authorization' => 'Bearer ' . $this->config['api_key'],
         ])->post($this->config['api_url'], $payload);
 
         info("Calling Laravel bug fix reporting >>>");
